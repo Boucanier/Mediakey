ed8a2cb9e3c650b683ec2d8dbf5d42fa879ee496
Jules Chiron <chironjules.78@gmail.com>
2024-12-13 17:59:46 -0500

    Change directory to execute python script

--- launch.bat
+++ launch.bat
@@ -1,13 +1,18 @@
 set PROJECT_PATH=%~dp0
 @echo off
+
+REM Move to the project directory
+cd /d %PROJECT_PATH%
+
 REM Check if the script is already running
 call "%PROJECT_PATH%.venv\Scripts\python.exe" "%PROJECT_PATH%scripts\check_running.py"
 if %errorlevel% equ 1 (
-    echo Script is already running
+    echo Mediakey is already running
     exit /b 1
 )
 
 REM Start the script in the background
+echo Starting Mediakey
 start "Mediakey" "%PROJECT_PATH%.venv\Scripts\pythonw.exe" "%PROJECT_PATH%src\mediakey.py" --ok
 
 exit /b 0
