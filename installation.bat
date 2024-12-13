06e81abe0d614ee1f0fe54733f89a812251de945
Jules Chiron <chironjules.78@gmail.com>
2024-12-13 14:50:44 -0500

    Windows installation file

---
+++ installation.bat
@@ -0,0 +1,68 @@
+@echo off
+
+REM Get the current project path
+set "PROJECT_PATH=%~dp0"
+
+REM Path to launch.bat
+set "LAUNCH_FILE=%~dp0launch.bat"
+
+REM Check if launch.bat exists
+if not exist %LAUNCH_FILE% (
+    echo %LAUNCH_FILE% not found
+    exit /b 1
+)
+
+REM Create a temporary file
+set "TEMP_FILE=%~dp0launch.tmp"
+
+REM Add line defining PROJECT_PATH to the temporary file
+echo set PROJECT_PATH=%PROJECT_PATH%> %TEMP_FILE%
+
+REM Remove all lines starting with "set PROJECT_PATH=" from launch.bat
+findstr /v /r "^set PROJECT_PATH=" %LAUNCH_FILE% > "%LAUNCH_FILE%.tmp"
+move /y "%LAUNCH_FILE%.tmp" %LAUNCH_FILE% > nul
+
+REM Add all lines from launch.bat to the temporary file
+type %LAUNCH_FILE% >> %TEMP_FILE%
+
+REM Replace launch.bat with the temporary file
+move /y %TEMP_FILE% %LAUNCH_FILE% > nul
+
+REM Define the Startup folder path
+set "STARTUP_FOLDER=%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+REM Copy launch.bat to the Startup folder
+echo Copying launch.bat to %STARTUP_FOLDER%
+copy /y %LAUNCH_FILE% "%STARTUP_FOLDER%" > nul
+
+REM Check if the copy was successful
+set "STARTUP_FILE=%STARTUP_FOLDER%\launch.bat"
+if exist "%STARTUP_FILE%" (
+    echo launch.bat added to Startup folder successfully.
+) else (
+    echo Failed to add launch.bat to Startup folder.
+    exit /b 1
+)
+
+REM Rename the file in the Startup folder
+set "NEW_NAME=%STARTUP_FOLDER%\mediakey_launcher.bat"
+REM Remove old file if it exists
+if exist "%NEW_NAME%" (
+    echo Removing old file %NEW_NAME%
+    del /f /q "%NEW_NAME%"
+)
+rename "%STARTUP_FILE%" "mediakey_launcher.bat"
+
+REM Verify if the renaming was successful
+if exist "%NEW_NAME%" (
+    echo File renamed to mediakey_launcher.bat successfully.
+) else (
+    echo Failed to rename the file in the Startup folder.
+    exit /b 1
+)
+
+REM Launch the script
+echo Running %NEW_NAME%
+call "%NEW_NAME%"
+
+exit /b 0
--- launch.bat
+++ launch.bat
@@ -1,10 +1,13 @@
+set PROJECT_PATH=%~dp0
 @echo off
 REM Check if the script is already running
-call "%~dp0.venv\Scripts\python.exe" scripts/check_running.py
+call "%PROJECT_PATH%.venv\Scripts\python.exe" "%PROJECT_PATH%scripts\check_running.py"
 if %errorlevel% equ 1 (
     echo Script is already running
-    exit /b
+    exit /b 1
 )
 
 REM Start the script in the background
-start "Mediakey" .venv/Scripts/pythonw.exe src/mediakey.py --ok
+start "Mediakey" "%PROJECT_PATH%.venv\Scripts\pythonw.exe" "%PROJECT_PATH%src\mediakey.py" --ok
+
+exit /b 0
