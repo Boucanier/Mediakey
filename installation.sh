aa2fbea850863d5f493ad6b4532f1bf537baa75e
Boucanier <chironjules.78@gmail.com>
2025-01-11 12:39:03 -0500

    Installation file

---
+++ installation.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Check if the script is run as root
+if [[ $EUID != 0 ]]
+then
+	echo This script must be run as root
+	exit 1
+fi
+
+# Install the necessary packages
+sudo apt install -y python3
+
+#  Install the virtual environment
+python3 -m venv .venv
+./.venv/bin/pip install -r requirements.txt
+
+# Create service
+echo "
+Description=Mediakey
+After=network.target
+
+[Service]
+ExecStart=sudo $(pwd)/.venv/bin/python3 $(pwd)/src/mediakey.py --ok
+Restart=always
+User=$SUDO_USER
+Group=$SUDO_USER
+
+[Install]
+WantedBy=multi-user.target" > mediakey.service
+
+sudo mv mediakey.service /etc/systemd/system/mediakey.service
+sudo systemctl enable mediakey.service
+sudo systemctl start mediakey.service
